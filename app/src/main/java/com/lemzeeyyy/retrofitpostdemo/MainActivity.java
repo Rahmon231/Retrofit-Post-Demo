48facdb416edf8c0e33f114ac25e53cd7480ed9a
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-10-02 01:13:24 +0100

    demo ui

--- app/src/main/java/com/lemzeeyyy/retrofitpostdemo/MainActivity.java
+++ app/src/main/java/com/lemzeeyyy/retrofitpostdemo/MainActivity.java
@@ -3,12 +3,21 @@ package com.lemzeeyyy.retrofitpostdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+    private TextView textView;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText1);
+        textView = findViewById(R.id.textView);
+        btn = findViewById(R.id.button);
     }
 }
